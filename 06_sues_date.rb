3cbac253b9bca269dff9931a57450b93a0415447
Seth Geyer <seth.geyer@gmail.com>
2014-06-19 16:45:57 -0600

    original commit

--- 06_sues_date.rb
+++ 06_sues_date.rb
@@ -1,3 +1,8 @@
 require_relative 'people'
 
 # What was the description of the last correspondence with sue?
+
+
+last_communication_description = PEOPLE["sue"][:correspondence][-1][:description]
+
+p last_communication_description
