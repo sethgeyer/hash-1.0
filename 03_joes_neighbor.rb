ff6e529074b7d7c3784e29b9a58aa1d00a9154e1
Seth Geyer <seth.geyer@gmail.com>
2014-06-19 15:17:56 -0600

    original commit

--- 03_joes_neighbor.rb
+++ 03_joes_neighbor.rb
@@ -3,3 +3,8 @@ require_relative 'people'
 # What is the last name of the person sitting next to joe?
 #
 # HINT: Start by finding who joe is sitting next to, then find that person in the hash.
+
+
+joes_neighbor = PEOPLE["joe"][:sitting_next_to].to_s
+
+p PEOPLE[joes_neighbor][:last_name]
